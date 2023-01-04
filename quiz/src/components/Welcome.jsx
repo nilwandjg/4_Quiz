d83c15a9b0a9272dd7f7c371d880b01895846518
nilwandjg <nilwandjg@yahoo.com.br>
2023-01-04 02:43:02 -0300

    Primeiro commit. Criação do componente Welcome.

---
+++ quiz/src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import Quiz from '../img/quiz.svg';
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+        <h2>Seja bem-vindo</h2>
+        <p>Clique no botão abaixo para começar:</p>
+        <button>Iniciar</button>
+        <img src={Quiz} alt="Início do Quiz" />
+    </div>
+  )
+}
+
+export default Welcome
